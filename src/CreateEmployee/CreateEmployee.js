358c218aa8fb8abcb5ce405df3f4a2ed0b99d244
Michael Landry <mlandryviolin@gmail.com>
2018-12-17 16:54:30 -0500

    add routing

---
+++ src/CreateEmployee/CreateEmployee.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class CreateEmployee extends Component {
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return (
+      <div className="CreateEmployee">
+      Create employee
+    
+      </div>
+    )
+  }
+}
+CreateEmployee.propTypes = {
+}
+
+export default CreateEmployee;
